7f423c09fc3a7ebe80d5fb680a96ee84a53f6d2a
Niket <niket1409@gmail.com>
2020-06-05 11:11:22 +0530

    Created a stub project

---
+++ JavaRefresher/src/main/java/com/JavaRefresher/JavaRefresherApplication.java
@@ -0,0 +1,13 @@
+package com.JavaRefresher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRefresherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaRefresherApplication.class, args);
+	}
+
+}
---
+++ JavaRefresher/src/test/java/com/JavaRefresher/JavaRefresherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JavaRefresher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRefresherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
